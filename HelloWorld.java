69b73ddee9e3ad02ec53fc3e867f78aa8e9f0348
NG0324 <2675391207@qq.com>
2022-10-17 18:40:19 +0800

    te

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package easy;
+
+class HelloWorld {
+    /*
+    下面的main函数才是程序的入口，main的格式(第七行代码)是固定的
+     */
+    public static void main(String[] args){//args是arguments：参数
+        //println表示输出并换行，print表示仅输出
+        System.out.println("Hello");
+        System.out.print("Hello");
+    }
+}
